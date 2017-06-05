8cfda45773cf1b64478ecf3384c83a840a67ca89
Christine Callo <christinejoann.callo@gmail.com>
2017-06-05 11:17:04 -0700

    Fix syntax error

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ alert(result);
 
 
 var subtract = function(number1, number2){
-  return  number1 + number2;
+  return  number1 - number2;
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
@@ -17,7 +17,7 @@ alert(result);
 
 
 var multiply = function(number1, number2){
-  return number1 + number2;
+  return number1 * number2;
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter annother number"));
@@ -25,7 +25,7 @@ var result = multiply(number1,number2);
 alert(result);
 
 var divide = function(number1, number2){
-  return = number1 + number2;
+  return = number1/number2;
 };
 var number1 = var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter annother number"));
